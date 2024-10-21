75236d16f21c240695efa5b7fe8cd3ff05fba6ae
inaciocr04 <inaciocr04@gmail.com>
2024-10-21 11:42:32 +0200

    update user.index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Tous les utilisateurs">
-    <ul class="space-y-4">
+    <!--    <ul class="space-y-4">
         @foreach($users as $user)
             <li class="bg-sixth-color px-8 py-6">{{$user->name}} / {{$user->email}}
                 <form action="{{ route('user.update', $user) }}" method="POST" class="inline">
@@ -15,6 +15,6 @@
                 </form>
             </li>
         @endforeach
-    </ul>
-
+    </ul>-->
+    <livewire:user-role />
 </x-layout>
