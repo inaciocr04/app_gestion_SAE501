8c5630aac0fee0e43fae22a99201471252df2124
inaciocr04 <inaciocr04@gmail.com>
2024-11-20 11:30:20 +0100

    modif migration student_status for id

--- database/migrations/2024_11_06_155754_student_status_table_add_id.php
+++ database/migrations/2024_11_06_155754_student_status_table_add_id.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::table('student_status', function (Blueprint $table) {
             $table->dropPrimary(['statut_id', 'student_id', 'actual_year_id', 'year_training_id', 'start_date_status']);
-            $table->bigInteger('id')->unsigned()->primary();
-            $table->bigInteger('id')->unsigned()->primary()->autoIncrement()->change();
+            $table->bigIncrements('id');
         });
     }
 
@@ -24,7 +23,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('student_status', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->primary(false)->autoIncrement(false)->change();
             $table->dropPrimary(['id']);
             $table->dropColumn('id');
             $table->primary(['statut_id', 'student_id', 'actual_year_id', 'year_training_id', 'start_date_status']);
